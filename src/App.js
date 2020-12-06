1c2b47a79858e9728c98cfa55dae84f4902cb6d7
Zoraida <zmcabrera@gmail.com>
2020-12-06 18:05:22 -0500

    feat: font scale and base color variables

--- src/App.js
+++ src/App.js
@@ -19,6 +19,15 @@ function App() {
         >
           Learn React
         </a>
+        <div className="light-background">
+        <h1>hello</h1>
+        <h2>hello</h2>
+        <h3>hello</h3>
+        <h4>hello</h4>
+        <h5>hello</h5>
+        <h6>hello</h6>
+        <p className="button-link">here is a <a>link</a></p>
+        </div>
       </header>
     </div>
   );
