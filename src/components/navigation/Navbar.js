7658decdbe3b3e506d64f56b1cfafa123728e95d
Zoraida <zmcabrera@gmail.com>
2020-12-13 13:10:54 -0500

    refractor: make Navbar into reusable component

---
+++ src/components/navigation/Navbar.js
@@ -0,0 +1,42 @@
+import logo from '../../images/icons/nav-logo.png'
+import exploreIcon from '../../images/icons/96px/search-white.png';
+import tripsIcon from '../../images/icons/96px/trips-outline-white.png';
+import addIcon from '../../images/icons/96px/add-outline-white.png';
+import profileIcon from '../../images/icons/96px/profile-outline-white.png';
+
+function Navbar () {
+
+    return (
+        <nav>
+            <div className={`container`}>
+                <div className={`logo`}>
+                    <img src={logo} alt={`Photo Atlas logo`}/>
+                </div>
+
+                <ul>
+                    <li>
+                        <img src={exploreIcon} alt/>
+                        <span>Explore</span>
+                    </li>
+
+                    <li>
+                        <img src={tripsIcon} alt/>
+                        <span>Trips</span>
+                    </li>
+
+                    <li>
+                        <img src={addIcon} alt/>
+                        <span>Add</span>
+                    </li>
+
+                    <li>
+                        <img src={profileIcon} alt/>
+                        <span>Profile</span>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+    )
+}
+
+export default Navbar;
