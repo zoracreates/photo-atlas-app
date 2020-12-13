2b37afa87a9a8056a95c50a4bb13ef2120e6c36c
Zoraida <zmcabrera@gmail.com>
2020-12-12 19:01:40 -0500

    feat: navbar has icons and text

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
-import logo from './logo.svg';
+import logo from './images/icons/nav-logo.png';
+import exploreIcon from './images/icons/96px/search-white.png';
+import tripsIcon from './images/icons/96px/trips-outline-white.png';
+import addIcon from './images/icons/96px/add-outline-white.png';
+import profileIcon from './images/icons/96px/profile-outline-white.png';
 import './styles/main.scss';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <img src="/icons/icon-72.png" alt=""/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="dark-background">
-        <h1>hello</h1>
-        <h2>hello</h2>
-        <h3>hello</h3>
-        <h4>hello</h4>
-        <h5>hello</h5>
-        <h6>hello</h6>
-        <p className="button-link">here is a <a>link</a></p>
+    <>
+      <nav>
+        <div className={`container`}>
+          <div className={`logo`}>
+            <img src={logo} alt={`Photo Atlas logo`}/>
+          </div>
+          <ul>
+            <li>
+              <img src={exploreIcon} alt/>
+              <span>Explore</span>
+            </li>
+
+            <li>
+              <img src={tripsIcon} alt/>
+              <span>Trips</span>
+            </li>
+            <li>
+              <img src={addIcon} alt/>
+              <span>Add</span>
+            </li>
+            <li>
+              <img src={profileIcon} alt/>
+              <span>Profile</span>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <header>
+        <div className={`container`}>
+          header
         </div>
       </header>
-    </div>
+      <main>
+        <div className={`container`}>
+          main
+        </div>
+      </main>
+
+
+    </>
   );
 }
 
