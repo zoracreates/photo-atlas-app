b74745a3287eb56f41ae64a6efa400604006381b
Zoraida <zmcabrera@gmail.com>
2020-12-21 22:09:40 -0500

    refractor: make photo slider into functional component, eliminate next and previous buttons, style scrollbar

--- src/components/slider/PhotoSlider.js
+++ src/components/slider/PhotoSlider.js
@@ -26,131 +26,51 @@ const imageList = [
 ];
 */
 
-class PhotoSlider extends React.Component {
 
-  state = {
-    scrollToEl: 2,
-    fwdToggle: 'active',
-    bckToggle: 'disabled'
+const windowWidth = window.innerWidth;
+const breakPoint = 600;
+const maxImageWidth = (windowWidth > breakPoint) ? (windowWidth / 2) : windowWidth;
+
+const PhotoSlider = (props) => {
+  const { imageList } = props;
+  const imagesCount = imageList.length;
+
+  const lightboxOptions = {
+    buttons: {
+      showDownloadButton: false,
+      showAutoplayButton: false
+    }
   }
 
-  render() {
+  return (
+    <SimpleReactLightbox>
+      <div className={`photo-slider`}>
 
-    const { imageList } = this.props;
-    const imagesCount = imageList.length;
+        <SRLWrapper options={lightboxOptions}>
 
-    const lightboxOptions = {
-      buttons: {
-        showDownloadButton: false,
-        showAutoplayButton: false
-      }
-    }
+          {imageList.map((image, id) => {
 
-    const windowWidth = window.innerWidth;
-    const maxImageWidth = (windowWidth > 600) ? (windowWidth / 2.5) : (windowWidth * .8);
+            const { src, alt } = image;
 
+            return (
+              <a key={id} href={src}>
+                <ContainedImage src={src} alt={alt} maxHeight={600} maxWidth={maxImageWidth} />
+              </a>
+            )
 
+          })}
 
-    let handleFwdClick = () => {
+        </SRLWrapper>
 
-      if (this.state.scrollToEl >= 1 && this.state.scrollToEl < imagesCount) {
+      </div>
 
-        let fwdTo = document.querySelector(`.photo-slider > div > a:nth-child(${this.state.scrollToEl})`);
+      <p className={`caption`}>Total images: {imagesCount}</p>
 
-        if (fwdTo) {
-          fwdTo.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
-        }
+    </SimpleReactLightbox>
 
-        this.setState(
-          prevState => { return { scrollToEl: prevState.scrollToEl + 1 } },
+  )
 
-          () => {
-            console.log(this.state.scrollToEl);
-            if (this.state.scrollToEl > 1 && this.state.bckToggle === 'disabled') {
-              this.setState({ bckToggle: 'active' })
-            }
-            if (this.state.scrollToEl >= imagesCount && this.state.scrollToEl) {
-              this.setState({ fwdToggle: 'disabled' })
-            }
-          }
-        )
-
-      }
-
-      else {
-        this.setState({ fwdToggle: 'disabled' })
-      }
-    }
-
-
-
-    let handleBckClick = () => {
-      if (this.state.scrollToEl > 1) {
-        this.setState(prevState => ({ scrollToEl: prevState.scrollToEl - 1 }),
-          () => {
-            let backTo = document.querySelector(`.photo-slider > div > a:nth-child(${this.state.scrollToEl})`)
-
-            if (backTo) {
-              backTo.scrollIntoView({ behavior: "smooth", block: "end", inline: "end" });
-            }
-
-            if (this.state.scrollToEl >= 1 && this.state.scrollToEl < imagesCount && this.state.fwdToggle === 'disabled') {
-              this.setState({ fwdToggle: 'active' })
-            }
-
-            if (this.state.scrollToEl === 1) {
-              this.setState(prevState => ({ scrollToEl: prevState.scrollToEl + 1 }),
-                () => {
-                  this.setState({ bckToggle: 'disabled' })
-                });
-            }
-
-
-            if (this.state.scrollToEl <= 2) {
-              this.setState({ bckToggle: 'disabled' })
-            }
-
-          });
-      }
-      else {
-        this.setState({ bckToggle: 'disabled' })
-      }
-    }
-
-
-    return (
-      <SimpleReactLightbox>
-        <div className={`photo-slider`}>
-          <button className={`${this.state.fwdToggle} fwd`} onClick={handleFwdClick}>
-            Next
-          </button>
-
-          <button className={`${this.state.bckToggle} bck`} onClick={handleBckClick}>
-            Previous
-          </button>
-          <SRLWrapper options={lightboxOptions}>
-            {imageList.map((image, id) => {
-
-              const { src, alt } = image;
-
-              return (
-                <a key={id} href={src}>
-                  <ContainedImage src={src} alt={alt} maxHeight={600} maxWidth={maxImageWidth} />
-                </a>
-              )
-
-            })}
-
-          </SRLWrapper>
-
-        </div>
-
-        <p className={`caption`}>Total images: {imagesCount}</p>
-
-      </SimpleReactLightbox>
-
-    )
-  }
 }
+
 
 export default PhotoSlider;
