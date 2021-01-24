54b016a20140ea5f8764956d0f09cccbbfb79f61
Zoraida <zmcabrera@gmail.com>
2021-01-24 13:19:44 -0500

    feat: create basic flickr API helper functions; refractor: create reusable Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <footer>
+            <div className={`container`}>
+                <p>&copy; PhotoAtlas 2021</p>
+                <p className={`caption`}>
+                    All photographs and their titles/metadata on PhotoAtlas are copyrighted and owned by
+                    their respective owners (the photographers) and not by PhotoAtlas. As such Photo Atlas does
+                    not grant any licenses to any copyrights, patents or any other intellectual property rights.
+          </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/utils/flickr/getFlickrPhotos.js
+++ src/utils/flickr/getFlickrPhotos.js
@@ -1,4 +1,4 @@
-async function requestFlickrPhotos(options = {}, handleResponse =
+async function getFlickrPhotos(options = {}, handleResponse =
     (responseData) => console.log(responseData)) {
     let url, item;
     url = `https://api.flickr.com/services/rest/?api_key=${process.env.REACT_APP_FLICKR_API_KEY}&method=flickr.photos.search&format=json&nojsoncallback=1}`
@@ -23,27 +23,30 @@ async function requestFlickrPhotos(options = {}, handleResponse =
         )
 };
 
-export default requestFlickrPhotos;
+export default getFlickrPhotos;
 
 /*
 USAGE
 
-The function requestFlickrPhotos receives two parameters options and handleResponse.
+The function getFlickrPhotos receives two parameters options and handleResponse.
 
 The first parameter, options, is an object that contains the parameters that need to be passed to the Flickr REST request. 
 
 The second parameter, handleResponse, is a callback indicating what to do once the request is received. 
 As a default handleResponse logs the received results into the browser console, but a different function may be passed. 
 
-As an example, the following call to requestFlickrPhotos requests a list of photos within Boston 
+As an example, the following call to getFlickrPhotos requests a list of photos within Boston  (defined by lat and lon), 
+along with their description and coordinates (defined in extas), sorted by how interesting they are
 and logs the data from the first photo in that list:
 
         let userInput = { 
             "lat": "42.3601",
-            "lon": "71.0589"
+            "lon": "-71.0589"
+            "extras" : "geo, description",
+            "sort" : "interestingness-desc"
         } 
 
-        requestFlickrPhotos(
+        getFlickrPhotos(
             userInput, 
             (data)=> {console.log(data.photos.photo[1])})
         );
