f1de83ec54fa0710b35ecaec6a65555dc6fe8419
Zoraida <zmcabrera@gmail.com>
2021-03-03 22:59:21 -0500

    chore: removed use of Google API due to billing issues

--- src/components/cards/LocationCard.js
+++ src/components/cards/LocationCard.js
@@ -1,89 +1,50 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import GoogleCalculateDistance from '../../utils/GoogleCalculateDistance';
 import { Link } from 'react-router-dom'
 
-class LocationCard extends React.Component {
 
-    state = {
-        distance: ""
+function LocationCard(props) {
+
+    let {
+        thumbnail,
+        title,
+        saves,
+        src,
+        locationId,
+        woeId } = props;
+    
+    if (!saves) {
+        saves = 0;
     }
 
-    render() {
-        let {
-            thumbnail,
-            title,
-            saves,
-            origin,
-            destination,
-            src,
-            locationId,
-            woeId } = this.props;
-        
-        let {distance} = this.state;
-
-        if (!saves) {
-            saves = 0;
-        }
-
-        if (!title) {
-            title = "Unamed Location"
-        }
-
-        let titleLength = title.length;
-
-        //keep the lenght of the title at 45 characters
-        if (titleLength > 45) {
-            let extraChars = 45 - titleLength;
-            let slicedTitle = title.slice(0, extraChars);
-            let lastSpace = slicedTitle.lastIndexOf(" ");
-            slicedTitle = slicedTitle.slice(0, lastSpace);
-            title = `${slicedTitle}...`
-        }
-
-        return (
-            <Link className={`location-card`} to={ `/location/${src}-${locationId}?woe=${woeId}`}>
-
-                <div className={`location-card-image`}>
-                    <img src={thumbnail} alt="" />
-                </div>
-                <div className={`location-card-content`}>
-                    <p className={`title`}>{title}</p>
-                    <p className={`meta-data saved`}>Saved by: {saves} {(saves > 1 || saves < 1) ? 'photographers' : 'photographer'}</p>
-                    {distance && <p className={`meta-data distance`}>Driving distance: {distance}</p>}
-                </div>
-                <GoogleCalculateDistance 
-                    destination={destination}
-                    origin={origin}
-
-                    handleResponse={
-                        (response) =>{
-                            let row, element, distance;
-                             
-                            row = response.rows[0]
-                             
-                             if(row) {
-                                 element = row.elements[0]
-                             }
-
-                             if(element) {
-                                 distance = element.distance
-                             }
-
-                            if(distance) {
-                                distance = distance.text
-                                this.setState({distance: distance}) 
-                            }
-                            
-                        }
-                    }
-
-                /> 
-
-            </Link>
-        )
-
+    if (!title) {
+        title = "Unamed Location"
     }
+
+    let titleLength = title.length;
+
+    //keep the lenght of the title at 45 characters
+    if (titleLength > 45) {
+        let extraChars = 45 - titleLength;
+        let slicedTitle = title.slice(0, extraChars);
+        let lastSpace = slicedTitle.lastIndexOf(" ");
+        slicedTitle = slicedTitle.slice(0, lastSpace);
+        title = `${slicedTitle}...`
+    }
+
+    return (
+        <Link className={`location-card`} to={ `/location/${src}-${locationId}?woe=${woeId}`}>
+
+            <div className={`location-card-image`}>
+                <img src={thumbnail} alt="" />
+            </div>
+            <div className={`location-card-content`}>
+                <p className={`title`}>{title}</p>
+                <p className={`meta-data saved`}>Saved by: {saves} {(saves > 1 || saves < 1) ? 'photographers' : 'photographer'}</p>
+            </div>
+
+        </Link>
+    )
 
 }
 
