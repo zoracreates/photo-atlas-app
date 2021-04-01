14c60233db574999a20e518c80f58fb31b8b3921
Zoraida <zmcabrera@gmail.com>
2021-03-31 20:12:07 -0400

    chore: removeextra JS in slider

--- src/components/slider/PhotoSlider.js
+++ src/components/slider/PhotoSlider.js
@@ -1,38 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import ContainedImage from './ContainedImage';
 import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
 
-
-
-/* 
-props.imageList receives an array of objects like this one:
-
-const imageList = [
-  {
-    src: "https://picsum.photos/700/800",
-    alt: "placeholder 1"
-  },
-  {
-    src: "https://picsum.photos/1000/300",
-    alt: "placeholder 2"
-  },
-  {
-    src: "https://picsum.photos/200/300",
-    alt: "placeholder 3"
-  },
-  {
-    src: "https://picsum.photos/2000/700",
-    alt: "placeholder 4"
-  }
-];
-*/
-
-
-const windowWidth = window.innerWidth;
-const breakPoint = 600;
-const maxImageWidth = (windowWidth > breakPoint) ? (windowWidth / 2) : windowWidth;
 
 const PhotoSlider = (props) => {
   const { imageList } = props;
@@ -58,7 +28,9 @@ const PhotoSlider = (props) => {
             return (
               <div key={id} className={`image-with-credit`}>
                 <a href={src}>
-                  <ContainedImage src={src} alt={alt} maxHeight={600} maxWidth={maxImageWidth} />
+                 <div className={`image-container`}>
+                 <img  src={src} alt={alt ? alt : ''}   />
+                 </div>
                 </a>
                 {author && <span className={`credit`}><a href={creditUrl}>{author}</a></span>}
               </div>
